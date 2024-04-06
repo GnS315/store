a3736c2e19f0a6f2ceb957710204aa145c30b5bf
Egor Morozov <e.d.morozov@esoft.local>
2024-04-06 12:02:35 +0500

    refactoring

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
+require('dotenv').config()
 
 const app = express()
 
-app.listen(5000, () => console.log('App is running'))
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
