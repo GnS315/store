73d13e1c73db3480beccfdf1256879ee89509abe
Egor Morozov <e.d.morozov@esoft.local>
2024-04-06 12:36:09 +0500

    app

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const router = require('./routes/index')
 require('dotenv').config()
 
 const app = express()
-
-
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+app.use('/api', router)
+
+try {
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}`)
+    })
+} catch(e) {
+    console.log(e)
+}
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const Router = require('express')
+const userRouter = require("./user");
+
+const router = new Router()
+
+router.use('/user', userRouter)
